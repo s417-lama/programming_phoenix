2f96dbec9ac3e61bab68c26e837144dfc1812905
slama <s417.lama@gmail.com>
2017-10-02 00:17:29 +0900

    finish chapter 4

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -9,4 +9,11 @@ defmodule Rumbl.User do
 
     timestamps()
   end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
